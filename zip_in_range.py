f4e5ccd8a60e8acb3276fbed5d2760cba274ae5a
Constantine Kurbatoff <6782841@gmail.com>
2020-09-24 20:15:54 -0400

    Second

--- zip_in_range.py
+++ zip_in_range.py
@@ -1,18 +1,23 @@
 from uszipcode import SearchEngine
 import argparse	
 
-
-
+# Default VERBOSITY level
+VERBOSITY = False
 
 def ZIPcodesInRange(zip,range,results=0):
+    #
     search = SearchEngine(simple_zipcode=True, db_file_dir="/tmp")  # set simple_zipcode=False to use rich info database
     zipcode = search.by_zipcode(zip)
-    print(zipcode)
-    print(zipcode.lat, zipcode.lng)
+    if VERBOSITY:
+        print('ZIP search results:')
+        print(zipcode)
+    if VERBOSITY:
+        print('Center coordiante: Lat:{} Long:{}'.format(zipcode.lat, zipcode.lng))
     res = search.by_coordinates(zipcode.lat, zipcode.lng, radius=range, returns=results)
     return res
 
 def ZIPcodesInRangeList(_zip,_range,_results=0):
+    #
     _data = []
     for zips in ZIPcodesInRange(_zip, _range, _results):
         _data.append(zips.zipcode)
@@ -20,7 +25,6 @@ def ZIPcodesInRangeList(_zip,_range,_results=0):
 
 
 def main():
-    print('—————————————————————————————————————')
     parser = argparse.ArgumentParser(description='Prints all zipcodes in range')
     parser.add_argument('zip', metavar='<zip code>', help='the base ZIP code for search ')
     parser.add_argument('--dist', dest='distance', type=int, 
@@ -29,8 +33,15 @@ def main():
     parser.add_argument('--lim', dest='limit', type=int, 
                     default=0,
                     help='limit results to number')
+    parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                    action="store_false")
+    VERBOSITY = args.verbosity;
+
+    if VERBOSITY:
+        print('—————————————————————————————————————')
     args = parser.parse_args()
-    print( 'ZIP:{}, Distance:{}'.format(args.zip, args.distance))
+    if VERBOSITY:
+        print( 'ZIP:{}, Distance:{}'.format(args.zip, args.distance))
     data = ZIPcodesInRangeList(args.zip, args.distance,args.limit)
     print( data )
 
